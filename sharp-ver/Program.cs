d7b9409fd39e67444d08a2292cee9763f45d2afa
dukemiller <dukemiller928@hotmail.com>
2017-01-11 14:02:32 -0700

    Initial commit

---
+++ sharp-ver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace sharp_ver
+{
+    public static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+        }
+
+    }
+}
