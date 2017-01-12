193c4c8a4d5f0bb70da5a6c61e46d81926a1dbf4
dukemiller <dukemiller928@hotmail.com>
2017-01-11 20:34:02 -0700

    Create some parsing models used logically in code

---
+++ sharp-ver/ParseArgs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace sharp_ver
+{
+    public class ParseArgs
+    {
+        public ParseArgs(IEnumerable<string> args)
+        {
+            var argList = args.Select(a => a.ToLower()).ToList();
+            Path = argList[0];
+            Tier = argList[1];
+            Action = argList[2];
+        }
+
+        public string Path { get; set; }
+        public string Tier { get; set; }
+        public string Action { get; set; }
+    }
+}
---
+++ sharp-ver/ParseResult.cs
@@ -0,0 +1,11 @@
+﻿namespace sharp_ver
+{
+    public class ParseResult
+    {
+        public bool Successful { get; set; }
+        public string Path { get; set; }
+        public SemanticVersionTier VersionTier { get; set; }
+        public SemanticAction Action { get; set; }
+        public override string ToString() => $"Result(Action={Action}, Tier={VersionTier}, Path={Path}, Successful={Successful})";
+    }
+}
